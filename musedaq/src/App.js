a765a4852b26d3585d3fb8f836180bb6682a5016
Natalie <theliteshado@icloud.com>
2021-05-25 03:27:30 -0400

    app.js

--- musedaq/src/App.js
+++ musedaq/src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+
+import { useState, useContext } from 'react'
+import { UserContext } from './contexts/UserContext'
+
+import { Route, Redirect } from 'react-router-dom'
+
+import Home from './pages/Home'
+import SignUpLogin from './components/SignupLogin'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Artists from './pages/Artists'
+import Label from './pages/Label'
+import Artist from './components/Artist'
 
 function App() {
+  const [user, setUser] = useContext(UserContext)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        <NavBar />
+      </div>
+
+      <Route
+       path="/"
+       exact
+       render={()=>{
+         return <Home />
+       }}
+      />
+
+<Route
+       path="/signup"
+       render={()=>{
+        if(user.id){
+          return <Redirect to ="/Artists" />
+        } else {
+         return <Signup />
+        }
+       }}
+      />
+
+      <Route
+       path="/login"
+       render={()=>{
+         if(user.id){
+          return <Redirect to ="/Artists" />
+         } else{
+          return <Login />
+         }
+       }}
+      />
+
+      <Route
+       path="/Artists"
+       render={()=>{
+         if(user.id){
+          return <Artists />
+         } else{
+          return <Redirect to ="/" />
+         }
+       }}
+      />
+
+      <Route
+       path="/Label"
+       render={()=>{
+         if(user.id){
+          return <Label />
+         } else{
+          return <Redirect to ="/" />
+         }
+       }}
+      />
+
+      <Route
+        path="/Artist/:id"
+        render={()=>{
+          if(user.id){
+            return <Artist />
+          }else{
+            return <Redirect to ="/" />
+          }
+        }}
+      />
+
+
     </div>
   );
 }
