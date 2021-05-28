d70daa58565a0d24073159dce24ae6de62c3cbed
Natalie <theliteshado@icloud.com>
2021-05-28 13:51:50 -0400

    ticker

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,24 @@ const Home = (props) => {
                     <p>_____________</p>
                     <h4>making independent artists' dreams come true.</h4>
                 </div>
+                <marquee class="ticker">
+                    <div class="ticker">
+                    <div class="ticker__item">INVB | Hip Hop</div>
+                    <div class="ticker__item percent">^2.1%</div>
+                    <div class="ticker__item">TARA | Pop </div>
+                    <div class="ticker__item percent">^5.7%</div>
+                    <div class="ticker__item">PP | RnB </div>
+                    <div class="ticker__item percentdown">`1.7%</div>
+                    <div class="ticker__item">SS | Pop </div>
+                    <div class="ticker__item percenthigh">^8.9%</div>
+                    <div class="ticker__item">XZ | Pop </div>
+                    <div class="ticker__item percent">^4.7%</div>
+                    <div class="ticker__item">RBOI | Rap </div>
+                    <div class="ticker__item percentdown">`3.7%</div>
+                    <div class="ticker__item">ISHN | RnB </div>
+                    <div class="ticker__item percenthigh">^7.7%</div>
+                    </div>
+                </marquee>
         </div>
     )
 }
